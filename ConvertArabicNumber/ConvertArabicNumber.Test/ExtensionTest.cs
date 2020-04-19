8da5a688834ccd8e1b2fd591a82ac50caaa04a03
oguzhancagliyan <oguzhancaglayan127@gmail.com>
2020-04-19 23:26:48 +0300

    Extension added. Possible Enums added. Tested for persian and arabic letter.

---
+++ ConvertArabicNumber/ConvertArabicNumber.Test/ExtensionTest.cs
@@ -0,0 +1,55 @@
+﻿using ConvertArabicNumber.Core;
+using System;
+using Xunit;
+
+namespace ConvertArabicNumber.Test
+{
+    public class ExtensionTest
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+
+        [Theory]
+        [InlineData("١٢٣٠")]
+        public void FindLang_When_Text_Is_Arabic_Or_Persian_ShouldReturn_Arabic_Or_Persian(string text)
+        {
+            var result = text.FindLang();
+
+            Assert.Equal(result.ToString(), LanguageForNumber.Persian.ToString());
+        }
+
+        [Theory]
+        [InlineData("123")]
+        public void FindLang_When_Text_Is_Arabic_Or_Persian_Should_Return_English(string text)
+        {
+            var result = text.FindLang();
+
+            Assert.Equal(result.ToString(), LanguageForNumber.English.ToString());
+        }
+
+        [Theory]
+        [InlineData("١٢٣٠", 1230)]
+        [InlineData("123", 123)]
+        public void GetArabicNumber_When_Text_Is_Arabic_Or_Persian_Should_Return_Arabic_Equality_Of_Text(string text, int expected)
+        {
+            var result = text.GetArabicNumber();
+
+            Assert.Equal(expected, result);
+
+        }
+
+        [Theory]
+        [InlineData("١٢٣٠١٢٣١", 12301231)]
+        [InlineData("1234444", 1234444)]
+        public void GetArabicNumber_When_Text_Is_Arabic_Or_Persian(string text, int expected)
+        {
+            var result = text.GetArabicNumber();
+
+            Assert.Equal(expected, result);
+
+        }
+    }
+}
---
+++ ConvertArabicNumber/ConvertArabicNumber/Extensions.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConvertArabicNumber.Core
+{
+    public static class Extensions
+    {
+        public static LanguageForNumber FindLang(this string text)
+        {
+            if (text.Any(c => c >= 0xFB50 && c <= 0xFEFC))
+            {
+                return LanguageForNumber.Arabic;
+            }
+            if (text.Any(c => c >= 0x0600 && c <= 0x06FF))
+            {
+                return LanguageForNumber.Persian;
+            }
+            if (text.Any(c => c >= 0x20 && c <= 0x7E))
+            {
+                return LanguageForNumber.English;
+            }
+            if (text.Any(c => c >= 0x2000 && c <= 0xFA2D))
+            {
+                return LanguageForNumber.Chinese;
+            }
+
+            return LanguageForNumber.English;
+        }
+
+        public static int GetArabicNumber(this string text)
+        {
+            int arabicNumber = 0;
+            var lang = text.FindLang();
+
+            if (lang == LanguageForNumber.English)
+            {
+                int.TryParse(text, out arabicNumber);
+            }
+            else if (lang == LanguageForNumber.Arabic || lang == LanguageForNumber.Persian)
+            {
+                arabicNumber = text.GetArabicNumberOfArabicLetter();
+            }
+
+            return arabicNumber;
+        }
+
+        public static int GetArabicNumberOfArabicLetter(this string text)
+        {
+            int arabicNumber = 0;
+            string values = string.Empty;
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (Char.IsDigit(text[i]))
+                {
+                    values += char.GetNumericValue(text, i);
+                }
+                else
+                {
+                    values += text[i].ToString();
+                }
+            }
+            int.TryParse(values, out arabicNumber);
+
+            return arabicNumber;
+        }
+    }
+}
